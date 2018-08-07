86040e0f2d1bf92df367de01fe9b90d3dd1e7eca
Craig Hutchinson <cdhutch@alumni.duke.edu>
2018-08-07 17:50:24 -0400

    git_redline clone repo's complete

--- git_redline.py
+++ git_redline.py
@@ -1,22 +1,37 @@
-# import subprocess
+import subprocess
 import sys
 import os
 import shutil
 
 
 class Repo(object):
-    def __init__(self, hash, path_to_redline_prefs, temp_dir, flag=False):
+    def __init__(self, hash, path_to_redline_prefs, temp_dir, url, subdir, build_py, fname_md, fname_csv, flag=False):
         self.hash = hash
-        self.flag = flag
         self.temp_dir = temp_dir
         self.prefs = path_to_redline_prefs
-        self.repo_path = os.path.join(temp_dir, 'git_redline', (flag + '_' + hash))
+        self.repo_path = os.path.join(
+            temp_dir, 'git_redline', (flag + '_' + hash))
+        self.url = url
+        self.subdir = subdir
+        self.build_py = build_py
+        self.fname_md = fname_md
+        self.fname_csv = fname_csv
+        self.flag = flag
 
     def clone(self):
         shutil.rmtree(self.repo_path, ignore_errors=True)
         os.makedirs(self.repo_path, exist_ok=True)
-        # clone_path =
-        pass
+        cwd_orig = os.getcwd()
+        os.chdir(self.repo_path)
+        subprocess.run(['git', 'clone', self.url])
+        str_cmd = 'git clone ' + self.url
+        print(str_cmd)
+        str_cmd = 'cd ' + subdir
+        print(subdir)
+        os.chdir(os.path.join(self.repo_path, subdir))
+        subprocess.run(['git', 'reset', '--hard', self.hash])
+        str_cmd = 'git reset --hard ' + self.hash
+        print(str_cmd)
 
 
 def parse_command_line(argv):
@@ -26,6 +41,12 @@ def parse_command_line(argv):
             Use python git_redline.py <path_to_redline_prefs.txt>
             <hash_from> <hash_to> <path_to_temporary_directory>''')
     path_to_redline_prefs = os.path.expanduser(argv[1])
+    f = open(path_to_redline_prefs)
+    url = f.readline().strip()
+    subdir = f.readline().strip()
+    build_py = f.readline()
+    fname_md = f.readline()
+    fname_csv = f.readline()
     hash_from = argv[2]
     hash_to = argv[3]
     if len(argv) == 5:
@@ -34,17 +55,19 @@ def parse_command_line(argv):
         path_to_temporary_directory = os.path.expanduser(
             '~/Documents/temporary')
     return (path_to_redline_prefs, hash_from,
-            hash_to, path_to_temporary_directory)
+            hash_to, path_to_temporary_directory, url, subdir,
+            build_py, fname_md, fname_csv)
 
 
 if __name__ == '__main__':
     (path_to_redline_prefs, hash_from, hash_to,
-        path_to_temporary_directory) = parse_command_line(
+        path_to_temporary_directory, url, subdir, build_py,
+        fname_md, fname_csv) = parse_command_line(
         sys.argv)
     repo_from = Repo(hash_from, path_to_redline_prefs,
-                     path_to_temporary_directory, 'from')
+                     path_to_temporary_directory, url, subdir, build_py, fname_md, fname_csv, 'from')
     repo_from.clone()
     # print(vars(repo_from))
     repo_to = Repo(hash_to, path_to_redline_prefs,
-                   path_to_temporary_directory, 'to')
+                   path_to_temporary_directory, url, subdir, build_py, fname_md, fname_csv, 'to')
     repo_to.clone()
